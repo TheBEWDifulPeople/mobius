9054ba8821f7bf92af607e284824f9de92bd16e1
Nikola Savic <nikolasavic@gmail.com>
2013-06-04 23:04:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mobius::Application.config.secret_key_base = 'be12eb59b22a42585bf9a53b1e24092e1f1563b2530847944bd4c07356f686d0aae3294ae614d629d1fc1dbc40f4a133e9e9f3da2796ec3a15c2ecba5bba5f53'
